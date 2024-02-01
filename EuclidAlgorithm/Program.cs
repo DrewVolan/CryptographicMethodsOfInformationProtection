7ea4e927296280ae07f24ddfeb102374759b0589
Andrew Voleyko <vandrewv16@gmail.com>
2024-02-01 18:45:46 +0300

    EuclidAlgorithm added

---
+++ EuclidAlgorithm/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+
+namespace EuclidAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите a: ");
+            var a = int.Parse(Console.ReadLine());
+            Console.Write("Введите b: ");
+            var b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"| q | r | x | y | a | b | x2 | x1 | y2 | y1 |");
+            Console.WriteLine($"| - | - | - | - | {a} | {b} | 1 | 0 | 0 | 1 |");
+            var result = EuclidAlgorithm(0, 0, 0, 0, a, b, 1, 0, 0, 1);
+            Console.WriteLine("d = {0}; x = {1}; y = {2}", result.d, result.x, result.y);
+            Console.ReadKey();
+        }
+
+        static EuclidAlgorithmResult EuclidAlgorithm(int q, int r, int x, int y, int a, int b, int x2, int x1, int y2, int y1)
+        {
+            q = Math.Abs(a / b);
+            r = a % b;
+            if (r != 0)
+            {
+                x = x2 - (q * x1);
+                y = y2 - (q * y1);
+                a = b;
+                b = r;
+                x2 = x1;
+                y2 = y1;
+                x1 = x;
+                y1 = y;
+                Console.WriteLine($"| {q} | {r} | {x} | {y} | {a} | {b} | {x2} | {x1} | {y2} | {y1} |");
+                return EuclidAlgorithm(q, r, x, y, a, b, x2, x1, y2, y1);
+            }
+            else
+            {
+                a = b;
+                b = r;
+                x2 = x1;
+                y2 = y1;
+                Console.WriteLine($"| {q} | {r} | X | X | {a} | {b} | {x2} | X | {y2} | X |");
+                return new EuclidAlgorithmResult(a, x2, y2);
+            }
+        }
+
+        struct EuclidAlgorithmResult
+        {
+            public int d;
+            public int x;
+            public int y;
+            
+            public EuclidAlgorithmResult(int d, int x, int y)
+            {
+                this.d = d;
+                this.x = x;
+                this.y = y;
+            }
+        }
+    }
+}
---
+++ EuclidAlgorithm/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("EuclidAlgorithm")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EuclidAlgorithm")]
+[assembly: AssemblyCopyright("Copyright © Волейко А. В., 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("8628eeb3-0a20-4945-a776-dcf384986143")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
